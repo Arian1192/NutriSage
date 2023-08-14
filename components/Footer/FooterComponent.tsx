a4c3463d755b5b2cff0d404ba75563bd9f81f3fd
Arian-Collaso <arian.collaso.rodriguez@gmail.com>
2023-08-14 12:29:14 +0200

    Fixed flex-grow section and text-sm footer

--- components/Footer/FooterComponent.tsx
+++ components/Footer/FooterComponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const FooterComponent = () => {
   return (
-    <div className="w-full h-64 flex flex-col gap-2 border bg-emerald-400 text-zinc-700 justify-center items-center">
+    <div className="w-full max-sm:text-sm h-64 flex flex-col gap-2 border bg-emerald-400 text-zinc-700 justify-center items-center">
       <p>Copyright 2023</p>
       <p>Diseñado para hacer mas saludable la vida de las personas.</p>
     </div>
